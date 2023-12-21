48e7b6754099b14f774f68b87b4d5514f20c4d21
Uttara Kamat <128808814+idlidosa1206@users.noreply.github.com>
2023-12-22 02:13:42 +0530

    Create RelatedProducts.js

---
+++ src/components/RelatedProducts/RelatedProducts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./RelatedProducts.css";
+
+const RelatedProducts = () => {
+  return (
+    <div className='relatedproducts'>
+        <h1></h1>
+    </div>
+  )
+}
+
+export default RelatedProducts
